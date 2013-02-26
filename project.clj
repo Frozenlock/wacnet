09b07da907a68173f59c93dc8a5ccbf2f006407a
Frozenlock <frozenlock@gmail.com>
2013-02-26 11:51:36 -0500

    don't throw an error on bacnet errors in messages

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject wacnet "0.1.1-BETA"
+(defproject wacnet "0.1.2-BETA"
   :description "Webserver to browse a BACnet network"
   :url "https://bacnethelp.com"
   :license {:name "GNU General Public License V3"
             :url "http://www.gnu.org/licenses/gpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.5.0-RC16"]
-                 [bacure "0.1.14"]
+                 [bacure "0.1.15"]
                  [ring "1.1.8"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.2"]
