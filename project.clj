67365d83adeba51b6941aad076c2940bdd954d70
Frozenlock <frozenlock@gmail.com>
2013-02-26 21:08:17 -0500

    don't throw error when dealing with not-yet-implemented methods

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject wacnet "0.1.3-BETA"
+(defproject wacnet "0.2.0-BETA"
   :description "Webserver to browse a BACnet network"
   :url "https://bacnethelp.com"
   :license {:name "GNU General Public License V3"
             :url "http://www.gnu.org/licenses/gpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.5.0-RC16"]
-                 [bacure "0.1.16"]
+                 [bacure "0.1.17"]
                  [ring "1.1.8"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.2"]
