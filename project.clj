3fb401f56f6c0c4bdc208bf7269a3c096c85fb83
Frozenlock <frozenlock@gmail.com>
2013-03-16 15:32:52 -0400

    up version

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject wacnet "1.0.0"
+(defproject wacnet "1.0.1"
   :description "Webserver to browse a BACnet network"
   :url "https://bacnethelp.com"
   :license {:name "GNU General Public License V3"
             :url "http://www.gnu.org/licenses/gpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [bacure "0.3.13"]
+                 [bacure "0.3.15"]
                  [ring "1.1.8"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.2"]
                  [lib-noir "0.3.5"]
-                 [org.clojars.frozenlock/logger "2.0.28"]]
+                 [org.clojars.frozenlock/logger "2.0.29"]]
   
   :main wacnet.server)
 
