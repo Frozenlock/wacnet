5a568648705f8fcd00e977e08ebdf546fb5e1d46
Frozenlock <frozenlock@gmail.com>
2014-05-13 01:38:52 -0400

    rebranding

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject wacnet "1.0.5"
+(defproject wacnet "1.0.6"
   :description "Webserver to browse a BACnet network"
-  :url "https://bacnethelp.com"
+  :url "https://hvac.io"
   :license {:name "GNU General Public License V3"
             :url "http://www.gnu.org/licenses/gpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
@@ -9,7 +9,7 @@
                  [compojure "1.1.5"]
                  [hiccup "1.0.2"]
                  [lib-noir "0.3.5"]
-                 [org.clojars.frozenlock/logger "2.0.34"]
+                 [org.clojars.frozenlock/logger "2.0.35"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [clojure-complete "0.2.3"] ;; clojure-complete is necessary for embedded nREPL
                  [trptcolin/versioneer "0.1.1"]]
