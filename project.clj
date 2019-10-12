983bc210ad8dedfdf7b8e170468ab344b3fe1cef
Frozenlock <frozenlock@gmail.com>
2019-10-12 14:27:13 -0400

    RC2, update dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject wacnet "2.1.6-RC1"
+(defproject wacnet "2.1.6-RC2"
   :description "Webserver to browse a BACnet network"
   :url "https://hvac.io"
   :license {:name "GNU General Public License V3"
             :url "http://www.gnu.org/licenses/gpl-3.0.html"}
   ;; WARNING
   ;; compiling with java 9 causes the webserver to hang when started on java 8
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
 
                  ;; BACnet
-                 [bacure "1.1.2"]
+                 [bacure "1.1.3"]
 
                  [io.hvac.vigilia/vigilia-logger "1.0.14"]
 
@@ -31,24 +31,25 @@
                  [cider/cider-nrepl "0.21.1"]
 
                  ;; cljs
-                 [org.clojure/clojurescript "1.10.339"]
-                 [org.clojure/core.async "0.4.474"]
-                 [org.clojure/tools.reader "1.3.0"]
+                 [org.clojure/clojurescript "1.10.520"]
+                 [org.clojure/core.async "0.4.500"]
+                 [org.clojure/tools.reader "1.3.2"]
                  [cljsjs/resize-observer-polyfill "1.4.2-0"]
 
                  [reagent "0.8.1"] ;; [reagent "0.8.0-alpha2"] ;; doesn't work with clojure 1.9 on java 9
                  
                  [org.clojars.frozenlock/reagent-modals "0.2.8"]
                  [org.clojars.frozenlock/reagent-keybindings "1.0.2"]
-                 [cljs-ajax "0.7.4"]
-                 [re-com "2.1.0" :exclusions [reagent]]
+                 [cljs-ajax "0.8.0"]
+                 [re-com "2.6.0"]
                  [com.andrewmcveigh/cljs-time "0.5.2"]
-                 [cljsjs/fixed-data-table-2 "0.7.17-2" ;; 0.8.11 has problems with 'scroll-to-row'
+                 [cljsjs/fixed-data-table-2 "0.8.23-0"
                   :exclusions [cljsjs/react]]]
 
   :plugins [[lein-environ "1.1.0"]
             [lein-ancient "0.6.15"]
             [lein-cljsbuild "1.1.7"]
+            [refactor-nrepl "2.4.0"]
             [org.clojars.rasom/lein-externs "0.1.7"]]
 
   :javac-options ["-target" "1.8" "-source" "1.8"] ;; retest later
@@ -91,13 +92,13 @@
 
                    :dependencies [[ring/ring-mock "0.3.2"]
                                   [ring/ring-devel "1.6.3"]
-                                  [figwheel-sidecar "0.5.18"]
+                                  [figwheel-sidecar "0.5.19"]
                                   [nrepl "0.6.0"]
-                                  [cider/piggieback "0.4.0"]]
+                                  [cider/piggieback "0.4.2"]]
 
                    :source-paths ["env/dev/clj"]
-                   :plugins [[lein-figwheel "0.5.18"]
-                             [cider/cider-nrepl "0.21.1"]]
+                   :plugins [[lein-figwheel "0.5.19"]
+                             [cider/cider-nrepl "0.22.4"]]
 
                    :figwheel {:http-server-root "public"
                               :readline false
