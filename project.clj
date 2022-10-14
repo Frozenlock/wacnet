a47c17da8198547749ad289b4e08c7f07a77139f
Frozenlock <frozenlock@gmail.com>
2022-10-14 16:06:45 -0400

    Version 2.1.8-RC4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wacnet "2.1.8-RC3"
+(defproject wacnet "2.1.8-RC4"
   :description "Webserver to browse a BACnet network"
   :url "https://hvac.io"
   :license {:name "GNU General Public License V3"
